226bd3fdf4ca207040bbf12951c74ff17326b1b3
Matthew Topham <mat.topham@gmail.com>
2017-02-28 13:35:01 +0000

    Added new WPF Messaging Client to redesign the WPF using MVVM.

--- MessagingClient/MainWindow.xaml.cs
+++ MessagingClient/MainWindow.xaml.cs
@@ -33,6 +33,7 @@ namespace MessagingClient
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void PollStreamForNew()
---
+++ MessagingClientMVVM/Views/LoginPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MessagingClientMVVM.Views
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService ns = NavigationService.GetNavigationService(this);
+            ns.Navigate(new Uri("Views/MessagingPage.xaml", UriKind.Relative));
+        }
+    }
+}
